44267de1e6d57139145ee95de692ad965437ff54
romeuhcf <romeu.hcf@gmail.com>
2014-12-03 10:03:46 -0200

    Fixing typo

--- ruby/processes_have_file_descriptors_closed_file.rb
+++ ruby/processes_have_file_descriptors_closed_file.rb
@@ -1,5 +1,5 @@
 # It's important to note that file descriptors keep track of open resources
-# only. Closed resoruces are not given a file descriptor number.
+# only. Closed resources are not given a file descriptor number.
 
 passwd = File.open("/etc/passwd")
 puts passwd.fileno
